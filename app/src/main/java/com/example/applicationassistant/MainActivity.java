615124b229db93c932558d6fbf549d1dac54733f
samyakgaur <samyakgaur@yahoo.com>
2019-10-23 02:59:56 +0530

    reset button added  || scrollview done || Current name displayed in right position

--- app/src/main/java/com/example/applicationassistant/MainActivity.java
+++ app/src/main/java/com/example/applicationassistant/MainActivity.java
@@ -12,10 +12,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    String[] names = new String[10];
+    String[] names = new String[100];
     EditText input;
+    int i =0;
+    int j =-1;
 
 //    private static final String TAG="MyMessage";
 
@@ -39,20 +42,55 @@ public class MainActivity extends AppCompatActivity {
         //help button end
         input = (EditText) findViewById(R.id.addnameinput);
         Button mybutton = (Button) findViewById(R.id.addnamebutton);
+        Button nextbutton = (Button) findViewById(R.id.next);
+        Button resetbutton = (Button) findViewById(R.id.reset);
 
         mybutton.setOnClickListener(
                 //creating a interface
                 new Button.OnClickListener(){
                     public void onClick(View v){
-                        names[0] = input.getText().toString();
-                        TextView mytext = (TextView) findViewById(R.id.change);
-                        mytext.setText(names[0]);
+                        names[i] = input.getText().toString();
+                        TextView tv=(TextView)findViewById(R.id.editText0);
+                        names[i]+="\n";
+                        tv.append(names[i]);
+                        tv.append(System.getProperty("line.separator"));
+                        i++;
+
                     }
 
                 }
 
 
         );
+        nextbutton.setOnClickListener(
+                //creating a interface
+                new Button.OnClickListener(){
+                    public void onClick(View v2){
+                        j=j+1;
+                        TextView mytext = (TextView) findViewById(R.id.change);
+                        mytext.setText(names[j]);
+                    }
+
+                }
+
+
+        );
+        resetbutton.setOnClickListener(
+                //creating a interface
+                new Button.OnClickListener(){
+                    public void onClick(View v2){
+                        j=0;
+                        TextView mytext = (TextView) findViewById(R.id.change);
+                        mytext.setText(names[j]);
+
+                    }
+
+                }
+
+
+        );
+
+
     }
 
 }
